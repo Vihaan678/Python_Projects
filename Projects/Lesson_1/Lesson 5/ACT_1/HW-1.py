c5035220760ee7b802a146e2240b9fb73035a517
Vihaan678 <vihaanagarwal607@gmail.com>
2025-08-31 18:21:05 +0530

    Class Projects

--- Projects/Lesson_1/Lesson 5/ACT_1/HW-1.py
+++ Projects/Lesson_1/Lesson 5/ACT_1/HW-1.py
@@ -1,5 +1,5 @@
 import random
-RED = "\033[91m"
+ROLL = "\033[91m"
 UNDERLINE = "\033[4m"
 RESET = "\033[0m"
 def roll_dice():
@@ -7,10 +7,10 @@ def roll_dice():
     picked = random.choice(sides)
     chance_of_six = sides.count('six') / len(sides)
     display_chance = f"{chance_of_six:.3f}"
-    print(f"{RED}Probability of landing on six: {UNDERLINE}{display_chance}{RESET}")
+    print(f"{ROLL}Probability of landing on six: {UNDERLINE}{display_chance}{RESET}")
     if picked == 'six':
-        return f"{RED}🎉 You got a six!{RESET}"
+        return f"{ROLL}🎉 You got a six!{RESET}"
     else:
-        return f"{RED}Nope, not a six this time.{RESET}"
+        return f"{ROLL}Nope, not a six this time. You can try roll{RESET}"
 result = roll_dice()
 print(result)
